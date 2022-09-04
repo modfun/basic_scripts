4be1e9416fe1a3b4f6e21dc0564198f6618c5606
modfun <modfun95@gmail.com>
2022-09-04 10:55:06 +0200

    simple git script

---
+++ git_done.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+fileName=$1
+cmtMsg=$2
+
+echo "Adding '$1' and commiting with '$2'."
+
+git add $1
+
+if [[ $? -eq 0 ]]
+then
+    git commit -m "$2"
+
+    if [[ $? -eq 0 ]]
+    then
+        echo "[Success] ready to push"
+    else
+        echo "[Failed] git commit -m '$2'."
+    fi
+else
+    echo "[Failed] git add $1"
+fi
+
+echo "[Finished]"
