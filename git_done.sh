873ec8de330f11b87d785ee89b10ff8405f7c16e
modfun <modfun95@gmail.com>
2022-09-09 14:40:56 +0200

    Finish gcc.sh and git_done.sh

--- git_done.sh
+++ git_done.sh
@@ -14,7 +14,7 @@ then
     if [[ $? -eq 0 ]]
     then
         echo "[Success] ready to push"
-        git log --graph --all --oneline --decorate
+        #git log --graph --all --oneline --decorate
     else
         echo "[Failed] git commit -m '$2'."
     fi
