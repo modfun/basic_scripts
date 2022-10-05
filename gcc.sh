6fdda27d692241d6b27db946db6967c6b2fcd8d7
modfun <modfun95@gmail.com>
2022-10-05 20:13:45 +0200

    Change gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -3,5 +3,5 @@ for x in $@
 do
     echo $x
     name=$(echo $x | rev | cut -d '.' -f 2- | rev)
-    gcc -o $name.out -v -ggdb -Wall -Werror -Wextra -pedantic -std=gnu89 $x | less
+    gcc -o $name.out -v -ggdb -Wall -Werror -Wextra -pedantic -std=gnu89 $x
 done
